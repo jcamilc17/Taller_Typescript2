24e1f70f4eed6bc821fc973686748e3649eb5adf
Juan Caldas <jc.caldas23@uniandes.edu.co>
2025-04-03 07:58:09 -0500

    🎨 Mejora mostrar info serie por defecto.

--- main.ts
+++ main.ts
@@ -1,63 +1,65 @@
-import {Serie} from './serie.js';
+import { Serie } from './serie.js';
 import { series } from './data.js';
 
 let componente: HTMLElement = document.getElementById('series')!;
 let componentePromedio: HTMLElement = document.getElementById('promedio')!;
-let foto:HTMLElement = document.getElementById('imagen')!;
-let titulo:HTMLElement = document.getElementById('titulo')!;
+let foto: HTMLElement = document.getElementById('imagen')!;
+let titulo: HTMLElement = document.getElementById('titulo')!;
 let descripcion: HTMLElement = document.getElementById('descripcion')!;
 let enlace: HTMLElement = document.getElementById('enlace')!;
 
+// Cargar las filas en la tabla
 cargarFilas();
+
+// Mostrar el promedio de temporadas
 componentePromedio.innerHTML = `Seasons average: ${calcularPromedio()}`;
+
+// Crear los eventos para los botones
 createBotones();
 
+// Mostrar por defecto la primera serie
+if (series.length > 0) {
+    infoSerie(series[0].id.toString());
+}
 
-
-function createBotones(){
-    series.forEach(c=>{
+function createBotones() {
+    series.forEach(c => {
         let boton = document.getElementById(`${c.id}`)!;
-        boton.onclick = () => {infoSerie(boton.id)};
+        boton.onclick = () => { infoSerie(boton.id) };
     });
 }
 
-function infoSerie(id: string){
+function infoSerie(id: string) {
     let idR: number = parseInt(id);
     let serie: Serie = series[idR - 1];
-    foto.setAttribute('src', serie.image);
-    titulo.innerHTML = `${serie.name}`;
-    descripcion.innerHTML = `${serie.description}`;
-    enlace.setAttribute('href', `${serie.url}`);
-    enlace.innerHTML = `${serie.url}`;
 
+    foto.setAttribute('src', serie.image);
+    titulo.innerHTML = serie.name;
+    descripcion.innerHTML = serie.description;
+    enlace.setAttribute('href', serie.url);
+    enlace.innerHTML = "More info";
 }
 
-function cargarFilas(): void{
+function cargarFilas(): void {
     series.forEach(c => crearFila(c));
 }
 
-function crearFila(serie: Serie):void{
+function crearFila(serie: Serie): void {
     let fila = document.createElement('tr');
-    fila.innerHTML = (`
-    <td style = "font-weight: bold;"> 
+    fila.innerHTML = `
+    <td style="font-weight: bold;"> 
         ${serie.id}
     </td>
-    <td style = "color:#547dde; hover">
-        <a id = "${serie.id}">${serie.name}</a>
+    <td style="color:#547dde; cursor: pointer;">
+        <a id="${serie.id}">${serie.name}</a>
     </td>
-    <td>
-        ${serie.channel}
-    </td>
-    <td>
-        ${serie.seasons}
-    </td>`);
+    <td>${serie.channel}</td>
+    <td>${serie.seasons}</td>`;
+    
     componente.appendChild(fila);
 }
 
-function calcularPromedio(): string{
-    let promedio = 0;
-    series.forEach(s => promedio += s.seasons);
-    promedio /= series.length;
-    let promedioTotal = promedio.toFixed(0);
-    return promedioTotal;
+function calcularPromedio(): string {
+    let promedio = series.reduce((sum, s) => sum + s.seasons, 0) / series.length;
+    return promedio.toFixed(2);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,16 @@ var foto = document.getElementById('imagen');
 var titulo = document.getElementById('titulo');
 var descripcion = document.getElementById('descripcion');
 var enlace = document.getElementById('enlace');
+// Cargar las filas en la tabla
 cargarFilas();
+// Mostrar el promedio de temporadas
 componentePromedio.innerHTML = "Seasons average: ".concat(calcularPromedio());
+// Crear los eventos para los botones
 createBotones();
+// Mostrar por defecto la primera serie
+if (series.length > 0) {
+    infoSerie(series[0].id.toString());
+}
 function createBotones() {
     series.forEach(function (c) {
         var boton = document.getElementById("".concat(c.id));
@@ -18,23 +25,20 @@ function infoSerie(id) {
     var idR = parseInt(id);
     var serie = series[idR - 1];
     foto.setAttribute('src', serie.image);
-    titulo.innerHTML = "".concat(serie.name);
-    descripcion.innerHTML = "".concat(serie.description);
-    enlace.setAttribute('href', "".concat(serie.url));
-    enlace.innerHTML = "".concat(serie.url);
+    titulo.innerHTML = serie.name;
+    descripcion.innerHTML = serie.description;
+    enlace.setAttribute('href', serie.url);
+    enlace.innerHTML = "More info";
 }
 function cargarFilas() {
     series.forEach(function (c) { return crearFila(c); });
 }
 function crearFila(serie) {
     var fila = document.createElement('tr');
-    fila.innerHTML = ("\n    <td style = \"font-weight: bold;\"> \n        ".concat(serie.id, "\n    </td>\n    <td style = \"color:#547dde; hover\">\n        <a id = \"").concat(serie.id, "\">").concat(serie.name, "</a>\n    </td>\n    <td>\n        ").concat(serie.channel, "\n    </td>\n    <td>\n        ").concat(serie.seasons, "\n    </td>"));
+    fila.innerHTML = "\n    <td style=\"font-weight: bold;\"> \n        ".concat(serie.id, "\n    </td>\n    <td style=\"color:#547dde; cursor: pointer;\">\n        <a id=\"").concat(serie.id, "\">").concat(serie.name, "</a>\n    </td>\n    <td>").concat(serie.channel, "</td>\n    <td>").concat(serie.seasons, "</td>");
     componente.appendChild(fila);
 }
 function calcularPromedio() {
-    var promedio = 0;
-    series.forEach(function (s) { return promedio += s.seasons; });
-    promedio /= series.length;
-    var promedioTotal = promedio.toFixed(0);
-    return promedioTotal;
+    var promedio = series.reduce(function (sum, s) { return sum + s.seasons; }, 0) / series.length;
+    return promedio.toFixed(2);
 }
